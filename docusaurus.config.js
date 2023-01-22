1d3c94ffdf436a9d22383405c04b9f0b2e25d83a
takahirokaji <tk69kj619@gmail.com>
2023-01-22 17:52:01 +0900

    create work flow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "unipp ドキュメント集",
   tagline: "unippに関するドキュメントの一覧です。",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://y-lab-dev.github.io/",
+  baseUrl: "/unipp-documents/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "y-lab-dev", // Usually your GitHub org/user name.
+  projectName: "unipp-documents", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
